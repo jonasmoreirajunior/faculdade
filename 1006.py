e87d2971754566dbb8de4ea7d9aa306eb7106aba
Jonas Moreira <jonas.om@outlook.com>
2018-02-17 00:36:15 -0200

    Arquivos .py

---
+++ 1006.py
@@ -0,0 +1,8 @@
+A = float(input())
+B = float(input())
+C = float(input())
+mediaA = A * 0.2
+mediaB = B * 0.3
+mediaC = C * 0.5
+media = mediaA + mediaB + mediaC
+print("MEDIA = %1.1f" %media)
---
+++ 1930.py
@@ -0,0 +1,7 @@
+tomadas = input().split()
+T1 = int(tomadas[0])
+T2 = int(tomadas[1])
+T3 = int(tomadas[2])
+T4 = int(tomadas[3])
+totalTomadas = (T1+T2+T3+T4)-3
+print(totalTomadas)
